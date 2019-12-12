a66d89f582d68ee0b70eed56024f3af58edd5021
offer365 <332000640@qq.com>
2019-12-12 23:18:35 +0800

    add More.md

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-home=$(
-  cd $(dirname $0)
-  pwd
-)
+home=$(cd $(dirname $0);pwd)
 chmod +x *.sh
 cp odin.service /usr/lib/systemd/system/
 sed -i "s#PATH#${home}#g" /usr/lib/systemd/system/odin.service
